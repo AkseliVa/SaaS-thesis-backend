41f3e5e50902d73ed3dda5bb3534137d2e75cab6
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2025-06-04 14:09:09 +0300

    first commit, db connection

---
+++ src/main/java/com/thesis/saas/SaaSApplication.java
@@ -0,0 +1,16 @@
+package com.thesis.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaSApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SaaSApplication.class, args);
+        System.out.println("Hello world");
+    }
+
+
+}
