280efefc7397a59b3c9d11ec302c45077b801cd1
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2025-06-05 09:26:24 +0300

    added repositories

---
+++ src/main/java/com/thesis/saas/admin/AdminRepository.java
@@ -0,0 +1,6 @@
+package com.thesis.saas.admin;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminRepository extends CrudRepository<Admin, Long> {
+}
---
+++ src/main/java/com/thesis/saas/company/CompanyRepository.java
@@ -0,0 +1,6 @@
+package com.thesis.saas.company;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, Long> {
+}
